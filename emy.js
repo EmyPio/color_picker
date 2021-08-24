9cc038e0bb054318516db58d47330608ab31aa2a
Emy Pio <89119758+EmyPio@users.noreply.github.com>
2021-08-24 16:19:31 +0530

    Add files via upload

---
+++ emy.js
@@ -0,0 +1,27 @@
+function changeyellow(){
+document.body.style.backgroundColor="yellow";
+}
+function changepink(){
+    document.body.style.backgroundColor="pink";
+    }
+function changeturquoise(){
+        document.body.style.backgroundColor="turquoise";
+        }
+     function changelime(){
+            document.body.style.backgroundColor="lime";
+            }
+            function changepurple(){
+                document.body.style.backgroundColor="purple";
+                }
+                function changelightsalmon(){
+                    document.body.style.backgroundColor="lightsalmon";
+                    }
+                function changered(){
+                        document.body.style.backgroundColor="red";
+                        }
+                     function changeorange(){
+                            document.body.style.backgroundColor="orange";
+                     }
+                            function changeblue(){
+                                document.body.style.backgroundColor="blue";
+                            }
